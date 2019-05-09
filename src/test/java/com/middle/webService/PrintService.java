8a5d3a63b381b46e9698cee813521f3d17315cf1
chen <18042690795@163.com>
2019-05-09 09:09:21 +0800

    5/8  中间库初版

---
+++ src/test/java/com/middle/webService/PrintService.java
@@ -0,0 +1,29 @@
+package com.middle.webService;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+/**
+ * @program: insurance
+ * @description:
+ * @author: Mr.Wang
+ * @create: 2019-04-29 11:33
+ **/
+@WebService
+public class PrintService {
+    public static void main(String[] args) {
+        String address = "http://localhost:9001/service/PrintName?wsdl";
+        Endpoint.publish(address, new PrintService());
+        System.out.println("--发布成功--");
+    }
+    //随便定义的方法
+    @WebMethod
+    public String findUserInfoByName(String arg0){
+
+        System.out.println("---------conn----------");
+        System.out.println(arg0+"111111111111111111111111111------------------------------------------------------------------------------");
+        return arg0;
+    }
+
+}
